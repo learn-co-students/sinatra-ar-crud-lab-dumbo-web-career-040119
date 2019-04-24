ea83df61a14e8b05be3d399259b45ec33324a69c
github username <github email address>
2019-04-23 23:17:52 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
     @article = Article.find_by_id(params[:id])
     @article.title = params[:title]
     @article.content = params[:content]
-    @article.save
+    @article.update
     redirect to "/articles/#{@article.id}"
   end
 
