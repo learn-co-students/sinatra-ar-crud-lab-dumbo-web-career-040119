f31e773bf46ab4fd009f35451b853f1d035d40b6
cogilvy <chrisogilvy@cox.net>
2019-04-23 20:08:50 -0400

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,20 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  # attr_accessor :title, :content
+  #
+  # @@all = []
+  #
+  # def initialize(title, content)
+  #   @title = title
+  #   @content = content
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
 end
+
+
+# YOU DONT NEED TO PUT ANYTHING IN HERE, ACTIVE RECORD DOES IT FOR YOU!!!!
